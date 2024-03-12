52965ee1ac5968f55e73e3e0201e3282ea641d26
Robson Chagas <112735714+robsonchagas@users.noreply.github.com>
2024-03-12 23:03:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+numero = int(input("Insira um número:"))
+dobro = numero * 2
+triplo = numero * 3
+raizQuadrada = numero ** (1/2)
+print("O dobro de", numero, "é", dobro)
+print("O triplo de", numero, "é", triplo)
+print("A raiz quadrada de", numero, "é", raizQuadrada)
